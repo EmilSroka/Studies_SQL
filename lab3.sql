eee20f0ae72d7f0cbe71931c2b16e706d41211f9
Emil Sroka <srokaemil@gmail.com>
2019-11-22 19:50:26 +0100

    Add solution to exercise 3.2

--- lab3.sql
+++ lab3.sql
@@ -24,3 +24,42 @@ WHERE EXTRACT(day FROM datarealizacji) BETWEEN 17 AND 19;
 -- 3.1.7 46 lub 47 tygodniu roku.
 SELECT idzamowienia, datarealizacji FROM public.zamowienia
 WHERE EXTRACT(week FROM datarealizacji) IN (46, 47);
+
+-- 3.2 (baza danych: cukiernia)Napisz zapytanie w języku SQL wyświetlające informacje na temat czekoladek 
+-- (idCzekoladki, nazwa, czekolada, orzechy, nadzienie), których nazwa:
+-- 3.2.1 rozpoczyna się na literę 'S',
+SELECT idczekoladki, nazwa, czekolada, orzechy, nadzienie 
+FROM public.czekoladki WHERE nazwa LIKE 'S%';
+-- 3.2.2 rozpoczyna się na literę 'S' i kończy się na literę 'i',
+SELECT idczekoladki, nazwa, czekolada, orzechy, nadzienie 
+FROM public.czekoladki WHERE nazwa LIKE 'S%i';
+-- 3.2.3 rozpoczyna się na literę 'S' i zawiera słowo rozpoczynające się na literę 'm',
+SELECT idczekoladki, nazwa, czekolada, orzechy, nadzienie 
+FROM public.czekoladki WHERE nazwa LIKE 'S_% m%';
+-- 3.2.4 rozpoczyna się na literę 'A', 'B' lub 'C',
+SELECT idczekoladki, nazwa, czekolada, orzechy, nadzienie 
+FROM public.czekoladki WHERE SUBSTR(nazwa, 1, 1) IN ('A', 'B', 'C');
+-- 3.2.5 zawiera rdzeń 'orzech' (może on wystąpić na początku i wówczas będzie pisany z wielkiej litery),
+SELECT idczekoladki, nazwa, czekolada, orzechy, nadzienie 
+FROM public.czekoladki WHERE nazwa ILIKE '%orzech%';
+-- 3.2.6 rozpoczyna się na literę 'S' i zawiera w środku literę 'm',
+SELECT idczekoladki, nazwa, czekolada, orzechy, nadzienie 
+FROM public.czekoladki WHERE nazwa LIKE 'S%m%_';
+-- 3.2.7 zawiera słowo 'maliny' lub 'truskawki',
+SELECT idczekoladki, nazwa, czekolada, orzechy, nadzienie FROM public.czekoladki 
+WHERE nazwa ILIKE '%maliny%' OR nazwa ILIKE '%truskawki%';
+-- OR
+SELECT idczekoladki, nazwa, czekolada, orzechy, nadzienie FROM public.czekoladki 
+WHERE nazwa SIMILAR TO '%((maliny)|(truskawki))%';
+-- 3.2.8 nie rozpoczyna się żadną z liter: 'D'-'K', 'S' i 'T',
+SELECT idczekoladki, nazwa, czekolada, orzechy, nadzienie FROM public.czekoladki 
+WHERE SUBSTR(nazwa, 1, 1) NOT IN ('D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'S', 'T');
+-- OR
+SELECT idczekoladki, nazwa, czekolada, orzechy, nadzienie FROM public.czekoladki 
+WHERE nazwa NOT SIMILAR TO '(D|E|F|G|H|I|J|K|S|T)%';
+-- 3.2.9 rozpoczyna się od 'Slod' ('Słod'),
+SELECT idczekoladki, nazwa, czekolada, orzechy, nadzienie 
+FROM public.czekoladki WHERE nazwa LIKE 'Slod%' OR nazwa LIKE 'Słod%';
+-- 3.2.10 składa się dokładnie z jednego słowa.
+SELECT idczekoladki, nazwa, czekolada, orzechy, nadzienie 
+FROM public.czekoladki WHERE POSITION(' ' IN nazwa) = 0;
